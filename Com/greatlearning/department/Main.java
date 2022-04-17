68920868fbe3ef88033545f55669428bf728547d
neha <chaudharyneha501@gmail.com>
2022-04-17 13:32:12 +0530

    assignment2

---
+++ Com/greatlearning/department/Main.java
@@ -0,0 +1,39 @@
+package Com.greatlearning.department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String greetings = "Welcome to ";
+		
+		AdminDepartment admin = new AdminDepartment();
+		HrDepartment hr = new HrDepartment();
+		TechDepartment tech = new TechDepartment();
+		
+		System.out.println( greetings + admin.departmentName() );
+		System.out.println( admin.getTodaysWork() );
+		System.out.println( admin.getWorkDeadline() );
+		System.out.println( admin.isTodayAHoliday() );
+		
+		System.out.println();
+		
+		System.out.println( greetings + hr.departmentName() );
+		System.out.println( hr.doActivity() );
+		System.out.println( hr.getTodaysWork() );
+		System.out.println( hr.getWorkDeadline() );
+		System.out.println( hr.isTodayAHoliday() );
+		
+		System.out.println();
+		
+		System.out.println( greetings + tech.departmentName() );
+		System.out.println( tech.getTodaysWork() );
+		System.out.println( tech.getWorkDeadline() );
+		System.out.println( tech.getTechStackInformation() );
+		System.out.println( tech.isTodayAHoliday() );
+		
+
+			}
+
+	}
+
---
+++ Com/greatlearning/department/SuperDepartment.java
@@ -0,0 +1,22 @@
+package Com.greatlearning.department;
+
+public class SuperDepartment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+			public String getTodaysWork() {
+				return "No Work as of now";
+			}
+			
+			public String getWorkDeadline() {
+				return "Nil";
+			}
+			
+			public String isTodayAHoliday() {
+				return "Today is not a holiday";
+				
+	
+					}
+
+		}
